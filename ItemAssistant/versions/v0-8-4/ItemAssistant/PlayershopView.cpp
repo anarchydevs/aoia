cedbc1c48b1c97699cd7cf6bae1d4ff9d51bba5e
myste <myste@c439a8fd-4c64-4eb0-a923-06793e67539c>
2008-03-31 17:39:44 +0000

    Tagged version 0.8.4

---
+++ ItemAssistant/versions/v0-8-4/ItemAssistant/PlayershopView.cpp
@@ -0,0 +1,181 @@
+#include "StdAfx.h"
+#include "PlayershopView.h"
+//#include <boost/filesystem/path.hpp>
+//#include <boost/filesystem/fstream.hpp>
+//#include <boost/filesystem/operations.hpp>
+
+
+enum ColumnID
+{
+    TEXT,
+    // this should be last always
+    COL_COUNT
+};
+
+PlayershopView::PlayershopView(void)
+: m_sortColumn(0)
+{
+}
+
+PlayershopView::~PlayershopView(void)
+{
+}
+
+
+LRESULT PlayershopView::OnColumnClick(LPNMHDR lParam)
+{
+    LPNMLISTVIEW pnmv = (LPNMLISTVIEW) lParam;
+
+    if (m_sortColumn != pnmv->iSubItem)
+    {
+        m_sortColumn = pnmv->iSubItem;
+        m_sortDesc = false;
+    }
+    else
+    {
+        m_sortDesc = !m_sortDesc;
+    }
+
+    m_listview.SortItemsEx(CompareStr, (LPARAM)this);
+
+    return 0;
+}
+
+LRESULT PlayershopView::OnItemActivate(LPNMHDR lParam)
+{
+    LPNMITEMACTIVATE param = (LPNMITEMACTIVATE)lParam;
+
+    int sel = param->iItem;
+    if (sel >= 0)
+    {
+        DWORD_PTR data = m_listview.GetItemData(sel);
+    }
+
+    return 0;
+}
+
+LRESULT PlayershopView::OnCreate(LPCREATESTRUCT createStruct)
+{
+   DWORD style = WS_CHILD | WS_VISIBLE | WS_CLIPSIBLINGS | WS_CLIPCHILDREN;
+
+   m_splitter.Create(m_hWnd, rcDefault, NULL, style);
+   m_splitter.SetSplitterExtendedStyle(0);
+
+   m_treeview.Create(m_splitter.m_hWnd, rcDefault, NULL, style | TVS_SHOWSELALWAYS | TVS_HASBUTTONS | TVS_HASLINES | TVS_LINESATROOT | TVS_EDITLABELS, WS_EX_CLIENTEDGE);
+   m_treeview.SetDlgCtrlID(IDW_PSMTREEVIEW);
+
+   m_listview.Create(m_splitter.m_hWnd, rcDefault, NULL, style | LVS_REPORT, WS_EX_CLIENTEDGE);
+   m_listview.SetDlgCtrlID(IDW_PSMLISTVIEW);
+   int colid = m_listview.AddColumn(_T("Info"), 0);
+   m_listview.SetColumnWidth(colid, 550);
+
+   colid = m_listview.AddColumn(_T("Toon"), 1);
+   m_listview.SetColumnWidth(colid, 100);
+
+   m_treeRoot.SetOwner(this);
+   m_treeview.SetRootItem(&m_treeRoot);
+   
+   m_splitter.SetSplitterPanes(m_treeview, m_listview);
+   m_splitter.SetActivePane(SPLIT_PANE_LEFT);
+
+   PostMessage(WM_POSTCREATE);
+
+   return 0;
+
+}
+
+
+void PlayershopView::UpdateLayout(CSize newSize)
+{
+    CRect r( CPoint( 0, 0 ), newSize );
+
+    m_splitter.SetWindowPos(NULL, r.left, r.top, r.Width(), r.Height(), SWP_NOZORDER | SWP_NOACTIVATE | SWP_DEFERERASE | SWP_NOSENDCHANGING);
+}
+
+
+LRESULT PlayershopView::OnSize(UINT wParam, CSize newSize)
+{
+    UpdateLayout(newSize);
+	return 0;
+}
+
+
+LRESULT PlayershopView::OnPostCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+{
+	//SetSplitterPos works best after the default WM_CREATE has been handled
+//	m_splitter.SetSplitterPos(200);
+
+	return 0;
+}
+
+// Updates the list view with the results of the SQL query. 'where' is used as the expression after the WHERE keyword.
+void PlayershopView::UpdateListView(std::tstring const& where)
+{
+   m_listview.DeleteAllItems();
+   m_listview.AddItem(0,0,where.c_str());
+}
+
+// Updates the list view with the results of the SQL query. 'where' is used as the expression after the WHERE keyword.
+void PlayershopView::UpdateListView(std::vector<std::tstring> v)
+{
+   m_listview.DeleteAllItems();
+
+   for(unsigned int i=0; i<v.size();i++){
+      m_listview.AddItem(0,i%2,v[i].c_str());
+   }
+}
+
+
+// Static callback function for sorting items.
+int PlayershopView::CompareStr(LPARAM param1, LPARAM param2, LPARAM sort)
+{
+   PlayershopView* pThis = (PlayershopView*)sort;
+
+   int result = 0;
+/*
+   TCHAR name1[MAX_PATH];
+   TCHAR name2[MAX_PATH];
+
+   ZeroMemory(name1, sizeof(name1));
+   ZeroMemory(name2, sizeof(name2));
+
+   pThis->m_listview.GetItemText(param1, pThis->m_sortColumn, name1, sizeof(name1) - 1);
+   pThis->m_listview.GetItemText(param2, pThis->m_sortColumn, name2, sizeof(name2) - 1);
+
+   switch (pThis->m_sortColumn)
+   {
+   case COL_QL:
+   case COL_STACK:
+      {
+         int a = atoi(to_ascii_copy(name1).c_str());
+         int b = atoi(to_ascii_copy(name2).c_str());
+         result = pThis->m_sortDesc ? b - a : a - b;
+      }
+      break;
+   default:
+      result = pThis->m_sortDesc ? StrCmpI(name2, name1) : StrCmpI(name1, name2);
+   }
+*/
+   return result;
+}
+
+
+void PlayershopView::OnActive(bool doActivation)
+{
+    if (doActivation && m_splitter.GetSplitterPos() < 0) {
+        m_splitter.SetSplitterPos(200);
+    }
+
+   CTreeItem item = m_treeview.GetRootItem();
+   item = m_treeview.GetChildItem(item);
+   if (!item.IsNull())
+   {
+      item = m_treeview.GetChildItem(item);
+   }
+   if (!item.IsNull())
+   {
+      m_treeview.SelectItem(item);
+   }
+
+
+}
---
+++ ItemAssistant/versions/v0-8-4/ItemAssistant/PsmTreeItems.cpp
@@ -0,0 +1,364 @@
+#include "stdafx.h"
+#include "PsmTreeItems.h"
+#include "PlayershopView.h"
+#include <sstream>
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/operations.hpp>
+//#include <boost/filesystem/exception.hpp>
+//#include <boost/iostreams/stream_buffer.hpp>
+//#include <boost/iostreams/stream.hpp>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#define TIXML_USE_STL
+#include <TinyXml/tinyxml.h>
+
+
+PsmTreeViewItemBase::PsmTreeViewItemBase(PlayershopView* pOwner)
+ : m_pOwner(pOwner)
+{
+}
+
+
+PsmTreeViewItemBase::~PsmTreeViewItemBase()
+{
+}
+
+
+void PsmTreeViewItemBase::SetOwner(PlayershopView* pOwner) 
+{
+   m_pOwner = pOwner;
+}
+
+
+unsigned int PsmTreeViewItemBase::AppendMenuCmd(HMENU hMenu, unsigned int firstID, WTL::CTreeItem item) const
+{
+   return firstID;
+}
+
+
+bool PsmTreeViewItemBase::HandleMenuCmd(unsigned int commandID, WTL::CTreeItem item)
+{
+   return false;
+}
+
+   
+void PsmTreeViewItemBase::SetLabel(std::tstring const& newLabel)
+{
+}
+
+void PsmTreeViewItemBase::OnSelected() 
+{
+}
+
+/***************************************************************************/
+/** Container Tree View Item                                              **/
+/***************************************************************************/
+
+AccountTreeViewItem::AccountTreeViewItem(
+   PlayershopView* pOwner, std::tstring accountName)
+ : m_label(accountName)
+ , PsmTreeViewItemBase(pOwner)
+{
+}
+
+
+AccountTreeViewItem::~AccountTreeViewItem()
+{
+}
+
+
+void AccountTreeViewItem::OnSelected() 
+{
+
+   m_pOwner->UpdateListView(GetAllSoldItems());
+}
+
+
+std::vector<std::tstring> AccountTreeViewItem::GetAllSoldItems()
+{
+   std::vector<PsmTreeViewItem*> children = GetChildren();
+   std::vector<std::tstring> v;
+   for(unsigned int i=0; i<children.size(); i++)
+   {
+      std::vector<std::tstring> items = ((CharacterTreeViewItem1*)children[i])->GetAllSoldItems();
+      for(unsigned int j=0; j<items.size(); j++)
+      {
+         v.push_back(items[j]);
+      }
+   }
+   return v;
+}
+
+bool AccountTreeViewItem::CanEdit() const
+{
+   return false;
+}
+
+
+std::tstring AccountTreeViewItem::GetLabel() const
+{
+   return m_label;
+}
+
+
+bool AccountTreeViewItem::HasChildren() const
+{
+   return true;
+}
+
+
+std::vector<PsmTreeViewItem*> AccountTreeViewItem::GetChildren() const
+{
+   std::vector<PsmTreeViewItem*> result;
+
+   std::tstring filename;
+   filename = STREAM2STR( g_DBManager.AOFolder() << _T("\\Prefs\\") << m_label);
+   boost::filesystem::path p(to_utf8_copy(filename), boost::filesystem::native);
+
+   boost::filesystem::path account(to_utf8_copy(filename), boost::filesystem::native);
+   boost::filesystem::directory_iterator character(account), filesEnd;
+
+   for (; character != filesEnd; ++character)
+   {
+      if(is_directory(*character)){
+         boost::filesystem::path logFile = (*character).root_path() / boost::filesystem::path("\\PlayerShopLog.html", boost::filesystem::native);
+         unsigned int charID = boost::lexical_cast<unsigned int>((*character).leaf().substr(4));
+         if(charID != 0)
+         {
+            result.push_back(new CharacterTreeViewItem1(m_pOwner, charID,this));
+            if(boost::filesystem::exists(logFile) && !boost::filesystem::is_directory(logFile)){
+               //we have a playershop file!!
+               //m_hasLogFile = true;
+            }
+         }  
+      }
+   }
+
+   return result;
+}
+
+
+unsigned int AccountTreeViewItem::AppendMenuCmd(HMENU hMenu, unsigned int firstID, WTL::CTreeItem item) const
+{
+   return firstID;
+}
+
+
+bool AccountTreeViewItem::HandleMenuCmd(unsigned int commandID, WTL::CTreeItem item)
+{
+   return false;
+
+}
+
+
+
+CharacterTreeViewItem1::CharacterTreeViewItem1(PlayershopView* pOwner, unsigned int charID, const AccountTreeViewItem* pParent)
+ : m_charid(charID)
+ , PsmTreeViewItemBase(pOwner)
+{
+   m_pParent = pParent;
+   std::tstring str = g_DBManager.GetToonName(m_charid);
+   if(str.empty()){
+      std::tstringstream ss;
+      ss << charID;
+      m_label = std::tstring(ss.str());
+   }else{
+      m_label = str;
+   }
+}
+
+
+CharacterTreeViewItem1::~CharacterTreeViewItem1()
+{
+}
+
+
+void CharacterTreeViewItem1::OnSelected() 
+{ 
+   
+   
+   m_pOwner->UpdateListView(GetAllSoldItems());
+
+}
+
+std::vector<std::tstring> CharacterTreeViewItem1::GetAllSoldItems()
+{
+   std::tstring filename;
+   filename = STREAM2STR( g_DBManager.AOFolder() << _T("\\Prefs\\") << m_pParent->GetLabel() << "\\Char" << m_charid << "\\PlayerShopLog.html");
+
+   boost::filesystem::path p(to_utf8_copy(filename),boost::filesystem::native);
+
+   std::ifstream in(p.string().c_str());
+   std::string line;
+   std::string text;
+   std::vector<std::tstring> v;
+   while(in){
+      line.clear();
+      std::getline(in,line);
+      if(!line.empty())
+      {
+         text += line;
+      }
+   }
+
+   // Now that we have the whole file, lets parse it
+
+   // Text located between the two following tags is to be considered an item sold
+   std::string startTag = "<div indent=wrapped>" ;
+   std::string endTag   = "</div>" ;
+
+   while(text.length() > 0)
+   {
+   
+      std::string::size_type start = text.find( startTag, 0 );
+      std::string::size_type end  = text.find( endTag , 0 );
+      if( start != std::string::npos && end != std::string::npos)
+      {
+         // adding data for column 1
+         v.push_back(from_ascii_copy(text.substr(start+startTag.length(),end-start-startTag.length())));
+
+         // adding data for column 2
+         std::tstring str = g_DBManager.GetToonName(m_charid);
+         if(str.empty()){
+            std::tstringstream ss;
+            ss << m_charid;
+            str = std::tstring(ss.str());
+         }
+         v.push_back(str);
+
+         // remove the already processed part of the string
+         text = text.substr(end+endTag.length());
+      }
+      else
+      {
+         text = "";
+      }
+   }
+   return v;
+}
+
+bool CharacterTreeViewItem1::CanEdit() const
+{
+   return false;
+}
+
+
+std::tstring CharacterTreeViewItem1::GetLabel() const
+{
+	return m_label;
+}
+
+
+void CharacterTreeViewItem1::SetLabel(std::tstring const& newLabel)
+{
+	m_label = newLabel;
+}
+
+
+bool CharacterTreeViewItem1::HasChildren() const
+{
+   return false;
+}
+
+
+std::vector<PsmTreeViewItem*> CharacterTreeViewItem1::GetChildren() const
+{
+   std::vector<PsmTreeViewItem*> result;
+
+   return result;
+}
+
+
+unsigned int CharacterTreeViewItem1::AppendMenuCmd(HMENU hMenu, unsigned int firstID, WTL::CTreeItem item) const
+{
+   return firstID;
+}
+
+
+bool CharacterTreeViewItem1::HandleMenuCmd(unsigned int commandID, WTL::CTreeItem item)
+{
+   return false;
+
+}
+
+
+
+PlayershopTreeRoot::PlayershopTreeRoot(PlayershopView* pOwner)
+ : PsmTreeViewItemBase(pOwner)
+{
+}
+
+
+PlayershopTreeRoot::~PlayershopTreeRoot()
+{
+}
+
+
+void PlayershopTreeRoot::OnSelected()
+{
+   std::vector<PsmTreeViewItem*> children = GetChildren();
+   std::vector<std::tstring> v;
+   for(unsigned int i=0; i<children.size(); i++)
+   {
+      std::vector<std::tstring> items = ((AccountTreeViewItem*)children[i])->GetAllSoldItems();
+      for(unsigned int j=0; j<items.size(); j++)
+      {
+         v.push_back(items[j]);
+      }
+   }
+
+   m_pOwner->UpdateListView(v);
+}
+
+
+bool PlayershopTreeRoot::CanEdit() const
+{
+   return false;
+}
+
+
+std::tstring PlayershopTreeRoot::GetLabel() const
+{
+   return _T("All Accounts");
+}
+
+
+bool PlayershopTreeRoot::HasChildren() const
+{
+   return true;
+}
+
+
+std::vector<PsmTreeViewItem*> PlayershopTreeRoot::GetChildren() const
+{
+   std::vector<PsmTreeViewItem*> result;
+   
+   std::tstring filename;
+   filename = STREAM2STR( g_DBManager.AOFolder() << _T("\\Prefs") );
+   if(filename.empty()){
+      return result;
+   }
+   boost::filesystem::path p(boost::filesystem::path(to_utf8_copy(filename), boost::filesystem::native));
+
+   boost::filesystem::directory_iterator account(p), dir_end;
+
+   if(!is_directory(p))
+   {
+      return result;
+   }
+
+   for (;account != dir_end; ++account)
+   {
+      boost::filesystem::path acc = *account;
+      if (is_directory(acc))
+	   { // we found an account ?
+         
+         result.push_back(new AccountTreeViewItem(m_pOwner, from_ascii_copy(acc.leaf())));
+	   }
+   }
+	
+   return result;
+}
+
